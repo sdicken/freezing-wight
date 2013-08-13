560ac952d567fa13070b888be9b656cb8f327aaa
Sam Dicken <sjdick04@louisville.edu>
2013-08-13 15:54:58 -0400

    Create HTML encoded message of names and URLs to be sent in email

--- fetcher.py
+++ fetcher.py
@@ -1,10 +1,11 @@
 import urllib2
 import os
-from postmark import PMMail
+#from postmark import PMMail
 from bs4 import BeautifulSoup
 # or if you're using BeautifulSoup4:
 # from bs4 import BeautifulSoup
 
+html_msg = ""
 page = 1
 url = 'http://www.legacy.com/obituaries/louisville/obituary-browse.aspx?page=' + str(page) + '&recentdate=0&entriesperpage=25'
 soup = BeautifulSoup(urllib2.urlopen(url).read())
@@ -13,15 +14,17 @@ while(soup.find(class_="obitName") != None):
     soup = BeautifulSoup(urllib2.urlopen(url).read())
     obitNames = soup.find_all(class_="obitName")
     for name in obitNames:
-        print(name.a.get_text())
-        print(name.a.get('href'))
+        html_msg += "<a href=\"" + name.a.get('href') + "\">" + name.a.get_text() + "</a><br />"
+        #print(name.a.get_text())
+        #print(name.a.get('href'))
     page += 1
+#print(html_msg)
 	
 message = PMMail(api_key = os.environ['POSTMARK_API_KEY'],
 				subject = "Daily Obits",
 				sender = "sjdick04@louisville.edu",
 				to = "sd.nfltitan@gmail.com",
-				text_body = "Testing",
+				html_body = html_msg,
 				tag = "none"
 				)
 message.send()
