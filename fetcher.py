467f1ae4e3074d21167509d7c7916bc001ccb73d
Sam Dicken <sjdick04@louisville.edu>
2013-08-13 15:43:45 -0400

    Roll back to Python2 due to Postmark import errors

--- fetcher.py
+++ fetcher.py
@@ -1,4 +1,4 @@
-import urllib.request
+import urllib2
 import os
 from postmark import PMMail
 from bs4 import BeautifulSoup
@@ -7,10 +7,10 @@ from bs4 import BeautifulSoup
 
 page = 1
 url = 'http://www.legacy.com/obituaries/louisville/obituary-browse.aspx?page=' + str(page) + '&recentdate=0&entriesperpage=25'
-soup = BeautifulSoup(urllib.request.urlopen(url).read())
+soup = BeautifulSoup(urllib2.urlopen(url).read())
 while(soup.find(class_="obitName") != None):
     url = 'http://www.legacy.com/obituaries/louisville/obituary-browse.aspx?page=' + str(page) + '&recentdate=0&entriesperpage=10'
-    soup = BeautifulSoup(urllib.request.urlopen(url).read())
+    soup = BeautifulSoup(urllib2.urlopen(url).read())
     obitNames = soup.find_all(class_="obitName")
     for name in obitNames:
         print(name.a.get_text())
