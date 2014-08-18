4d33d5c5937ae605d8532a37d276293ff8d9fffc
Sam Dicken <sjdick04@louisville.edu>
2014-08-17 20:28:13 -0400

    Apply 2to3 tool to fetcher.py to automatically fix some issues.

--- fetcher.py
+++ fetcher.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import os
 from postmark import PMMail
 from bs4 import BeautifulSoup
@@ -9,7 +9,7 @@ html_msg = ""
 page = 1
 
 url = 'http://www.legacy.com/obituaries/louisville/obituary-browse.aspx?recentdate=0&type=1'
-soup = BeautifulSoup(urllib2.urlopen(url).read())
+soup = BeautifulSoup(urllib.request.urlopen(url).read())
 while(soup.find(class_="obitName") != None):
     #url = 'http://www.legacy.com/obituaries/louisville/obituary-search.aspx?page=' + str(page) + '&affiliateid=1175&countryid=0&daterange=1&stateid=20&townname=louisville&keyword=&entriesperpage=50'
     #soup = BeautifulSoup(urllib2.urlopen(url).read())
@@ -19,13 +19,13 @@ while(soup.find(class_="obitName") != None):
         #print(name.a.get_text())
         #print(name.a.get('href'))
     page += 1
-#print(html_msg)
+print(html_msg)
 	
-message = PMMail(api_key = os.environ['POSTMARK_API_KEY'],
-				subject = "Daily Obits",
-				sender = "sjdick04@louisville.edu",
-				to = "kandjdicken@gmail.com",
-				html_body = html_msg,
-				tag = "none"
-				)
-message.send()
+#message = PMMail(api_key = os.environ['POSTMARK_API_KEY'],
+#				subject = "Daily Obits",
+#				sender = "sjdick04@louisville.edu",
+#				to = "a874188@trbvm.com",
+#				html_body = html_msg,
+#				tag = "none"
+#				)
+#message.send()
