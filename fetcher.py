d1ec70653dad003daf832a4c74fdb9ed46ad3a66
Sam Dicken <sjdick04@louisville.edu>
2014-08-17 21:27:03 -0400

    Refactor using Legacy.com web services API rather than scraping HTML.

--- fetcher.py
+++ fetcher.py
@@ -1,31 +1,38 @@
 import urllib.request, urllib.error, urllib.parse
 import os
+import json
 from postmark import PMMail
-from bs4 import BeautifulSoup
-# or if you're using BeautifulSoup4:
-# from bs4 import BeautifulSoup
 
 html_msg = ""
 page = 1
-
-url = 'http://www.legacy.com/obituaries/louisville/obituary-browse.aspx?recentdate=0&type=1'
-soup = BeautifulSoup(urllib.request.urlopen(url).read())
-while(soup.find(class_="obitName") != None):
-    #url = 'http://www.legacy.com/obituaries/louisville/obituary-search.aspx?page=' + str(page) + '&affiliateid=1175&countryid=0&daterange=1&stateid=20&townname=louisville&keyword=&entriesperpage=50'
-    #soup = BeautifulSoup(urllib2.urlopen(url).read())
-    obitNames = soup.find_all(class_="obitName")
-    for name in obitNames:
-        html_msg += "<a href=\"" + name.a.get('href') + "\">" + name.a.get_text() + "</a><br />"
-        #print(name.a.get_text())
-        #print(name.a.get('href'))
+params = urllib.parse.urlencode({'type': 1, 'page':1})
+params = params.encode('utf-8')
+url = 'http://www.legacy.com/obituaries/louisville/api/obituary/0?type=1&ln=&page=' + str(page)
+request = urllib.request.Request(url)
+request.add_header("Content-Type","application/json;charset=utf-8")
+f = urllib.request.urlopen(request)
+#print(f.read().decode('utf-8'))
+response = f.read().decode('utf-8')
+data = json.loads(response)
+while(data["NumPageRemaining"] != 0):
+    url = 'http://www.legacy.com/obituaries/louisville/api/obituary/0?type=1&ln=&page=' + str(page)
+    request = urllib.request.Request(url)
+    request.add_header("Content-Type","application/json;charset=utf-8")
+    f = urllib.request.urlopen(request)
+    response = f.read().decode('utf-8')
+    data = json.loads(response)
+    entries = data["Entries"]
+    for entry in entries:
+        html_msg += "<a href=\"" + entry["obitlink"] + "\">" + entry["name"] + "</a><br />"
+        #print(entry["name"])
     page += 1
 print(html_msg)
 	
-#message = PMMail(api_key = os.environ['POSTMARK_API_KEY'],
-#				subject = "Daily Obits",
-#				sender = "sjdick04@louisville.edu",
-#				to = "a874188@trbvm.com",
-#				html_body = html_msg,
-#				tag = "none"
-#				)
-#message.send()
+message = PMMail(api_key = os.environ['POSTMARK_API_KEY'],
+				subject = "Daily Obits",
+				sender = "sjdick04@louisville.edu",
+				to = "kandjdicken@gmail.com",
+				html_body = html_msg,
+				tag = "none"
+				)
+message.send()
