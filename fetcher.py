f9f47d49fcfd6972d3a5cf11ae7170c09614650d
Sam Dicken <sjdick04@louisville.edu>
2014-08-20 15:23:28 -0400

    Use the search feature to narrow down obituary results.

--- fetcher.py
+++ fetcher.py
@@ -7,7 +7,7 @@ html_msg = ""
 page = 1
 params = urllib.parse.urlencode({'type': 1, 'page':1})
 params = params.encode('utf-8')
-url = 'http://www.legacy.com/obituaries/louisville/api/obituary/0?type=1&ln=&page=' + str(page)
+url = 'http://www.legacy.com/obituaries/louisville/api/obituarysearch?&affiliateid=1175&countryid=1&daterange=1&stateid=20&townname=Louisville&keyword=&entriesperpage=15&page=' + str(page)
 request = urllib.request.Request(url)
 request.add_header("Content-Type","application/json;charset=utf-8")
 f = urllib.request.urlopen(request)
@@ -15,7 +15,7 @@ f = urllib.request.urlopen(request)
 response = f.read().decode('utf-8')
 data = json.loads(response)
 while(data["NumPageRemaining"] > -1):
-    url = 'http://www.legacy.com/obituaries/louisville/api/obituary/0?type=1&ln=&page=' + str(page)
+    url = 'http://www.legacy.com/obituaries/louisville/api/obituarysearch?&affiliateid=1175&countryid=1&daterange=1&stateid=20&townname=Louisville&keyword=&entriesperpage=15&page=' + str(page)
     request = urllib.request.Request(url)
     request.add_header("Content-Type","application/json;charset=utf-8")
     f = urllib.request.urlopen(request)
